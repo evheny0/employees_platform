33afccffc0736b845d7a89fdafbf9d3040728798
Eugene Haisenka <evheny0@gmail.com>
2021-12-09 19:00:08 +0300

    Describe controller work

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ For this task you are given a list of employees. The output of the task is an en
 
 # Code description
 
-Entry point is API controller [`Api::V1::EmployeesController`](https://github.com/evheny0/employees_platform/blob/master/app/controllers/api/v1/employees_controller.rb)
+Entry point is API controller [`Api::V1::EmployeesController`](https://github.com/evheny0/employees_platform/blob/master/app/controllers/api/v1/employees_controller.rb)[(spec)](https://github.com/evheny0/employees_platform/blob/master/spec/requests/api/v1/employees_spec.rb). This controller gets a list of employees using [`EmployeesRepository#all`](https://github.com/evheny0/employees_platform/blob/master/app/repositories/employees_repository.rb). For every employee controller fetch country info using [`ApiClients::Restcountries`](https://github.com/evheny0/employees_platform/blob/master/lib/api_clients/restcountries.rb) and convert into JSON for response using [Employees::List::Decorator](https://github.com/evheny0/employees_platform/blob/master/app/domains/employees/list/decorator.rb).
--- spec/lib/api_clients/restcountries_spec.rb
+++ spec/lib/api_clients/restcountries_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 RSpec.describe ApiClients::Restcountries do
-  let(:responce_it) { [{"name" => {"common" => "Italy","official" => "Italian Republic","nativeName" => {"ita" => {"official" => "Repubblica italiana","common" => "Italia"}}},"tld" => [".it"],"cca2" => "IT","ccn3" => "380","cca3" => "ITA","cioc" => "ITA","independent" => true,"status" => "officially-assigned","unMember" => true,"currencies" => {"EUR" => {"name" => "Euro","symbol" => "€"}},"idd" => {"root" => "+3","suffixes" => ["9"]},"capital" => ["Rome"],"altSpellings" => ["IT","Italian Republic","Repubblica italiana"],"region" => "Europe","subregion" => "Southern Europe","languages" => {"ita" => "Italian"},"translations" => {},"latlng" => [42.83333333,12.83333333],"landlocked" => false,"borders" => ["AUT","FRA","SMR","SVN","CHE","VAT"],"area" => 301336.0,"demonyms" => {"eng" => {"f" => "Italian","m" => "Italian"},"fra" => {"f" => "Italienne","m" => "Italien"}},"flag" => "","maps" => {"googleMaps" => "https://goo.gl/maps/8M1K27TDj7StTRTq8","openStreetMaps" => "https://www.openstreetmap.org/relation/365331"},"population" => 59554023,"gini" => {"2017" => 35.9},"fifa" => "ITA","car" => {"signs" => ["I"],"side" => "right"},"timezones" => ["UTC+01:00"],"continents" => ["Europe"],"flags" => {"png" => "https://flagcdn.com/w320/it.png","svg" => "https://flagcdn.com/it.svg"},"coatOfArms" => {"png" => "https://mainfacts.com/media/images/coats_of_arms/it.png","svg" => "https://mainfacts.com/media/images/coats_of_arms/it.svg"},"startOfWeek" => "monday","capitalInfo" => {"latlng" => [41.9,12.48]},"postalCode" => {"format" => "#####","regex" => "^(\\d{5})$"}}] }
+  let(:response_it) { [{"name" => {"common" => "Italy","official" => "Italian Republic","nativeName" => {"ita" => {"official" => "Repubblica italiana","common" => "Italia"}}},"tld" => [".it"],"cca2" => "IT","ccn3" => "380","cca3" => "ITA","cioc" => "ITA","independent" => true,"status" => "officially-assigned","unMember" => true,"currencies" => {"EUR" => {"name" => "Euro","symbol" => "€"}},"idd" => {"root" => "+3","suffixes" => ["9"]},"capital" => ["Rome"],"altSpellings" => ["IT","Italian Republic","Repubblica italiana"],"region" => "Europe","subregion" => "Southern Europe","languages" => {"ita" => "Italian"},"translations" => {},"latlng" => [42.83333333,12.83333333],"landlocked" => false,"borders" => ["AUT","FRA","SMR","SVN","CHE","VAT"],"area" => 301336.0,"demonyms" => {"eng" => {"f" => "Italian","m" => "Italian"},"fra" => {"f" => "Italienne","m" => "Italien"}},"flag" => "","maps" => {"googleMaps" => "https://goo.gl/maps/8M1K27TDj7StTRTq8","openStreetMaps" => "https://www.openstreetmap.org/relation/365331"},"population" => 59554023,"gini" => {"2017" => 35.9},"fifa" => "ITA","car" => {"signs" => ["I"],"side" => "right"},"timezones" => ["UTC+01:00"],"continents" => ["Europe"],"flags" => {"png" => "https://flagcdn.com/w320/it.png","svg" => "https://flagcdn.com/it.svg"},"coatOfArms" => {"png" => "https://mainfacts.com/media/images/coats_of_arms/it.png","svg" => "https://mainfacts.com/media/images/coats_of_arms/it.svg"},"startOfWeek" => "monday","capitalInfo" => {"latlng" => [41.9,12.48]},"postalCode" => {"format" => "#####","regex" => "^(\\d{5})$"}}] }
 
   describe "#by_code" do
     let(:return_value) { ApiClients::Restcountries::CountryInfo.new(region: "Europe", common_name: "Italy", currencies: ["EUR"], languages:["ita"], timezones: ["UTC+01:00"]) }
 
     it "make request to restcountries" do
-      expect(HTTParty).to receive(:get).with("https://restcountries.com/v3.1/alpha/IT").and_return(responce_it)
+      expect(HTTParty).to receive(:get).with("https://restcountries.com/v3.1/alpha/IT").and_return(response_it)
       expect(described_class.new.by_code("IT")).to eq(return_value)
     end
   end
@@ -16,7 +16,7 @@ RSpec.describe ApiClients::Restcountries do
     let(:return_value) { ApiClients::Restcountries::CountryInfo.new(region: "Europe", common_name: "Italy", currencies: ["EUR"], languages:["ita"], timezones: ["UTC+01:00"]) }
 
     it "make request to restcountries only once for certain country" do
-      expect(HTTParty).to receive(:get).with("https://restcountries.com/v3.1/alpha/IT").and_return(responce_it)
+      expect(HTTParty).to receive(:get).with("https://restcountries.com/v3.1/alpha/IT").and_return(response_it)
       described_class.instance.by_code("IT")
 
       expect(HTTParty).not_to receive(:get)
@@ -24,10 +24,10 @@ RSpec.describe ApiClients::Restcountries do
     end
 
     it "make request to restcountries for other country" do
-      expect(HTTParty).to receive(:get).with("https://restcountries.com/v3.1/alpha/SP").and_return(responce_it)
+      expect(HTTParty).to receive(:get).with("https://restcountries.com/v3.1/alpha/SP").and_return(response_it)
       described_class.instance.by_code("SP")
 
-      expect(HTTParty).to receive(:get).with("https://restcountries.com/v3.1/alpha/other_country").and_return(responce_it)
+      expect(HTTParty).to receive(:get).with("https://restcountries.com/v3.1/alpha/other_country").and_return(response_it)
       described_class.instance.by_code("other_country")
     end
   end
