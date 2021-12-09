d99d8e41be87e0a6111de84f5c9162b3ba8cd5dd
Eugene Haisenka <evheny0@gmail.com>
2021-12-09 18:22:41 +0300

    More examples for id generation

--- spec/domains/employees/list/regional_id_factory_spec.rb
+++ spec/domains/employees/list/regional_id_factory_spec.rb
@@ -3,10 +3,29 @@ require "rails_helper"
 RSpec.describe Employees::List::RegionalIdFactory do
   describe "#call" do
     let(:employee) { instance_double(EmployeesRepository::Employee, firstName: "firstName", lastName: "lastName", dateOfBirth: "11/11/1111") }
-    let(:country) { instance_double(ApiClients::Restcountries::CountryInfo, region: "Europe") }
 
-    it "generates special id for Europe" do
-      expect(described_class.new(employee, country).call).to eq("firstnamelastname11/11/1111")
+    context "Europe" do
+      let(:country) { instance_double(ApiClients::Restcountries::CountryInfo, region: "Europe") }
+
+      it "generates special id" do
+        expect(described_class.new(employee, country).call).to eq("firstnamelastname11/11/1111")
+      end
+    end
+
+    context "Asia" do
+      let(:country) { instance_double(ApiClients::Restcountries::CountryInfo, region: "Asia") }
+
+      it "generates special id" do
+        expect(described_class.new(employee, country).call).to eq("firstnamelastname11/11/1111")
+      end
+    end
+
+    context "Africa" do
+      let(:country) { instance_double(ApiClients::Restcountries::CountryInfo, region: "Africa") }
+
+      it "returns nil" do
+        expect(described_class.new(employee, country).call).to be_nil
+      end
     end
   end
 end
